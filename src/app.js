463da615eeb0ded45dc016697f373f71d0313572
jeong922 <hwajeong922@naver.com>
2025-08-28 15:05:56 +0900

    chore: three.js 및 Vite 설치

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 export default class App {
   constructor(target) {
     this.target = target;
+    this.render();
+  }
+
+  template() {
+    return `<div class="container">test</div>`;
+  }
+
+  mounted() {}
+
+  render() {
+    this.target.innerHTML = this.template();
+    this.mounted();
   }
 }
