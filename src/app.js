f978ab100cd736c9e150c617419914311df69b40
jeong922 <hwajeong922@naver.com>
2025-09-02 16:24:32 +0900

    feat: 기본 component 클래스 추가

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
+import Canvas from './universe.js';
+import Component from './component.js';
 import APOD from './api/api.js';
 
-export default class App {
-  constructor(target) {
-    this.target = target;
-    this.render();
+export default class App extends Component {
+  constructor(target, props) {
+    super(target, props);
   }
 
   template() {
-    return `<div class="container">
-          <canvas class="canvas"></canvas>
-    </div>`;
+    return `<div class="container"></div>`;
   }
 
   async mounted() {
     try {
-      const data = await APOD();
-      console.log('APOD 데이터:', data);
+      // const data = await APOD();
+      // console.log('APOD 데이터:', data);
+      const container = this.target.querySelector('.container');
+      const universe = new Canvas(container);
     } catch (err) {
       console.error(err);
     }
