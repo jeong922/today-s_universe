90fb7ffcb861edfd84c7e694d1844c53d9aa4c41
jeong922 <hwajeong922@naver.com>
2025-09-30 18:31:11 +0900

    feat: Title 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import { Title } from './components/Title';
 
 function App() {
+  const [visible, setVisible] = useState(true);
+
+  const handleStart = () => {
+    setVisible(false);
+  };
+
   return (
     <div className='bg-black w-full min-h-screen'>
-      <Title />
+      {visible && <Title onStart={handleStart} />}
+      {!visible && <div className='text-white'>여기 우주 들어감</div>}
     </div>
   );
 }
